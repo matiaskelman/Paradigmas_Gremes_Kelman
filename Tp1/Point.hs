0d8bc1d8866a9b0f45bf93379c1f543c6069da42
matiaskelman <88509593+matiaskelman@users.noreply.github.com>
2023-08-17 09:09:08 -0300

    Add files via upload

---
+++ Tp1/Point.hs
@@ -0,0 +1,8 @@
+module Point ( Point, newP, difP)
+   where
+
+data Point = Poi Int Int deriving (Eq, Show) -- Poi es una funcion que recibe 2 Int.
+
+newP :: Int -> Int -> Point -- Nuevo Punto
+newP x y= x + y
+difP :: Point -> Point -> Float  -- distancia absoluta (entre puntos)
---
+++ Tp1/Tp1.hs
@@ -0,0 +1,6 @@
+--Como importamos modulos
+--data Quality = Qua String Int Float deriving (Eq, Show) que significa (Eq, Show)
+--No entedemos bien que recibe quality en los parametros.
+module Main ()
+    where
+import Point
