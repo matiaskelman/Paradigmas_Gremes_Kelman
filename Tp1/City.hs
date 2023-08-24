668f676c71b8a69c1eb6ea2bc38a20a8bddcd8fe
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-08-23 21:06:50 -0300

    Update City.hs

--- Tp1/City.hs
+++ Tp1/City.hs
@@ -1,6 +1,6 @@
-module City (City (..), newC, nameC, distanceC) where
+module City (City , newC, nameC, distanceC) where
 
-import Point (Point (..), difP, newP)
+import Point (Point , difP, newP)
 
 data City = Cit String Point deriving (Eq, Show) -- Los objetos del type ciudad reciben un string y un point.
 
@@ -13,12 +13,15 @@ nameC (Cit nombre _) = nombre
 distanceC :: City -> City -> Float -- Le pasas 2 ciudades, de las ciudades toman el point y entre esos points hace difP.
 distanceC (Cit _ posicion1) (Cit _ posicion2) = difP posicion1 posicion2
 
+punto1 = newP 3 0
+punto2 = newP 5 0
+
 t :: [Bool]
 t =
-  [ newC "Igna" (Poi 1 0) == Cit "Igna" (Poi 1 0),
-    newC "Mati" (Poi 0 1) == Cit "Mati" (Poi 0 1),
-    nameC (Cit "Emilio Crack" (Poi 0 0)) == "Emilio Crack",
-    distanceC (Cit "Igna" (Poi 1 0) ) (Cit "Mati" (Poi 0 1)) == difP (Poi 1 0) (Poi 0 1),
-    distanceC (Cit "Igna" (Poi 4 0) ) (Cit "Mati" (Poi 3 0)) == 1,
+  [ newC "Igna" punto1 == Cit "Igna" punto1,
+    newC "Mati" punto2 == Cit "Mati" punto2,
+    nameC (Cit "Emilio Crack" punto1) == "Emilio Crack",
+    distanceC (Cit "Igna" punto1) (Cit "Mati" punto2) == difP punto1 punto2,
+    distanceC (Cit "Igna" punto1 ) (Cit "Mati" punto2) == 2,
     True
-  ]
+    ]
