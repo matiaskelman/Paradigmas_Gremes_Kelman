078932f2ce5a9cfdfbfbadc1aaa4851a731f4eb9
matiaskelman <88509593+matiaskelman@users.noreply.github.com>
2023-08-27 16:06:02 -0300

    Update Test.hs

--- Tp1/Test.hs
+++ Tp1/Test.hs
@@ -2,52 +2,39 @@ module Test () where
 
 import City (City, distanceC, nameC, newC)
 import Data.Text.Array (new)
+import Distribution.Simple.Test (test)
 import Link (Link, capacityL, connectsL, delayL, linksL, newL)
 import Point (Point, difP, newP)
 import Quality (Quality, capacityQ, delayQ, newQ)
-import Region (Region, availableCapacityForR, connectedR, delayR, foundR, linkR, linkedR, newR, tunelR)
+--import Region (Region, availableCapacityForR, connectedR, delayR, foundR, linkR, linkedR, newR, tunelR)
 import Tunel (Tunel, connectsT, delayT, newT, usesT)
 
-link1 :: Link
-link1 = newL ciudad1 ciudad2 calidad1
+t :: [[Bool]]
+t = [testCity, testLink, testPoint, testQuality, testTunel]
 
-link2 :: Link
-link2 = newL ciudad2 ciudad3 calidad2
-
-link3 :: Link
-link3 = newL ciudad3 ciudad1 calidad2
-
-region1 :: Region
-region1 = tunelR (linkR (foundR newR ciudad1) ciudad1 ciudad2 calidad1) [ciudad1, ciudad2]
-
-region2 :: Region
-region2 = tunelR (linkR (foundR newR ciudad1) ciudad1 ciudad2 calidad1) [ciudad1, ciudad2]
+-- region1 :: Region
+-- region1 = tunelR (linkR (foundR newR ciudad1) ciudad1 ciudad2 calidad1) [ciudad1, ciudad2]
+-- region2 :: Region
+-- region2 = tunelR (linkR (foundR newR ciudad1) ciudad1 ciudad2 calidad1) [ciudad1, ciudad2]
 
 punto1 :: Point
 punto1 = newP 1 0
-
 punto2 :: Point
 punto2 = newP 0 1
-
 punto3 :: Point
 punto3 = newP 2 0
-
 testPoint :: [Bool]
 testPoint =
   [ difP punto1 punto2 == sqrt 2,
     difP punto1 punto3 == 1,
     True
   ]
-
 ciudad1 :: City
 ciudad1 = newC "Igna" punto1
-
 ciudad2 :: City
 ciudad2 = newC "Mati" punto2
-
 ciudad3 :: City
 ciudad3 = newC "Emilio Capo!" punto3
-
 testCity :: [Bool]
 testCity =
   [ nameC ciudad1 == "Igna",
@@ -57,13 +44,10 @@ testCity =
     distanceC ciudad1 ciudad3 == 1,
     True
   ]
-
 calidad1 :: Quality
 calidad1 = newQ "Bronce" 1 2.0
-
 calidad2 :: Quality
 calidad2 = newQ "Aluminio" 20 1.0
-
 testQuality :: [Bool]
 testQuality =
   [ capacityQ calidad1 == 1,
@@ -71,7 +55,12 @@ testQuality =
     delayQ calidad1 == 2.0,
     True
   ]
-
+link1 :: Link
+link1 = newL ciudad1 ciudad2 calidad1
+link2 :: Link
+link2 = newL ciudad2 ciudad3 calidad2
+link3 :: Link
+link3 = newL ciudad3 ciudad1 calidad2
 testLink :: [Bool]
 testLink =
   [ newL ciudad1 ciudad2 calidad1 == link1,
@@ -82,13 +71,10 @@ testLink =
     delayL link1 == 2.0,
     True
   ]
-
 tunel1 :: Tunel
 tunel1 = newT [link1, link2]
-
 tunel2 :: Tunel
 tunel2 = newT [link3, link1]
-
 testTunel :: [Bool]
 testTunel =
   [ connectsT ciudad1 ciudad3 tunel1,
