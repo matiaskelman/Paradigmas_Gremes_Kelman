bf6105001388204f20a004aa02db36996896f069
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-09-15 00:55:06 -0300

    Update Contenedor.java

--- Tp2/src/queue/Contenedor.java
+++ Tp2/src/queue/Contenedor.java
@@ -2,6 +2,12 @@ package queue;
 
 abstract class Contenedor {
 	
+//	public Object contenido = 0;
+//	
+//	Contenedor(Object contenido) {
+//		this.contenido = contenido;
+//	};
+	
 	public abstract boolean isEmpty();
 
 	public abstract Object take();
