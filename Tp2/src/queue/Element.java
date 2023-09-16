e8c29712ac6b76635c840d0a72004b8b77e35839
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-09-16 17:01:02 -0300

    Terminado

--- Tp2/src/queue/Element.java
+++ Tp2/src/queue/Element.java
@@ -1,12 +1,14 @@
 package queue;
 
-abstract class Contenedor {
+import java.util.List;
+
+abstract class Element {
 	
 	public Object content;
 
 	public abstract boolean isEmpty();
 
-	public abstract Object take();
+	public abstract Object take(List<Element> list);
 
 	public abstract Object head();
 }
