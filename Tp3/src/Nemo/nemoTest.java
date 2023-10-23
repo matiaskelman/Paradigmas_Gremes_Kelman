eb5f240ebe738cf25c32d28fc70a67184d40aaea
Ignacio Gremes <igremes@udesa.edu.ar>
2023-10-22 22:17:02 -0300

    completamente terminado

--- Tp3/src/Nemo/nemoTest.java
+++ Tp3/src/Nemo/nemoTest.java
@@ -8,203 +8,158 @@ import static org.junit.jupiter.api.Assertions.*;
 public class nemoTest {
 	@Test
 	public void test01NewSubmarine() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
-        assertSame(sub.getCoordinates(), coordenadasIniciales);
-        assertSame(sub.getDirection(), direccionInicial);
+		Coordinates initialCoordinates = new Coordinates(1, 2);
+		String initialDirection = "North";
+		Sub sub = new Sub(initialCoordinates, initialDirection);
+        assertSame(sub.getCoordinates(), initialCoordinates);
+        assertSame(sub.getDirection(), initialDirection);
 	}
 
 	@Test
 	public void test02NullCommands() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Coordinates initialCoordinates = new Coordinates(1, 2);
+		String initialDirection = "North";
+		Sub sub = new Sub(initialCoordinates, initialDirection);
 		sub.action("");
-        assertSame(sub.getCoordinates(), coordenadasIniciales);
-        assertSame(sub.getDirection(), direccionInicial);
+        assertSame(sub.getCoordinates(), initialCoordinates);
+        assertSame(sub.getDirection(), initialDirection);
 	}
 
 	@Test
 	public void test03Descend() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "North");
 		sub.action("d");
         assertEquals(-1, sub.getDepth());
 	}
 
 	@Test
 	public void test04Ascend() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "North");
 		sub.action("ddu");
         assertEquals(-1, sub.getDepth());
 	}
 
 	@Test
 	public void test05AscendOnSurface() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
-		assertThrowsLike(() -> sub.action("u"), DepthZero.ERROR_MESSAGE);
+		assertThrowsLike(() -> new Sub(new Coordinates(1, 2), "North").action("u"), DepthZero.ERROR_MESSAGE);
 	}
 
 	@Test
 	public void test06RotateNorthRight() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "North");
 		sub.action("r");
-        assertSame("East", sub.getDirection());
+		assertEquals("East", sub.getDirection());
 	}
 
 	@Test
 	public void test07RotateNorthLeft() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "North");
 		sub.action("l");
-		assertSame("West", sub.getDirection());
+		assertEquals("West", sub.getDirection());
 	}
 
 	@Test
 	public void test08RotateSouthRight() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "South";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "South");
 		sub.action("r");
-		assertSame("West", sub.getDirection());
+		assertEquals("West", sub.getDirection());
 	}
 
 	@Test
 	public void test09RotateSouthLeft() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "South";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "South");
 		sub.action("l");
-		assertSame("East", sub.getDirection());
+		assertEquals("East", sub.getDirection());
 	}
 
 	@Test
 	public void test10RotateEastRight() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("r");
-		assertSame("South", sub.getDirection());
+		assertEquals("South", sub.getDirection());
 	}
 
 	@Test
 	public void test11RotateEastLeft() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("l");
-        assertSame("North", sub.getDirection());
+		assertEquals("North", sub.getDirection());
 	}
 
 	@Test
 	public void test12RotateWestRight() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "West";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "West");
 		sub.action("r");
-		assertSame("North", sub.getDirection());
+		assertEquals("North", sub.getDirection());
 	}
 
 	@Test
 	public void test13RotateWestLeft() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "West";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "West");
 		sub.action("l");
-		assertSame("South", sub.getDirection());
+		assertEquals("South", sub.getDirection());
 	}
 
 	@Test
 	public void test14Rotate() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("lr");
-        assertSame("East", sub.getDirection());
+		assertEquals("East", sub.getDirection());
 	}
 
 	@Test
 	public void test15AdvanceEast() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("f");
         assertEquals(2, sub.getCoordinates().getX());
 	}
 
 	@Test
 	public void test16AdvanceWest() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "West";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "West");
 		sub.action("f");
 		assertEquals(0, sub.getCoordinates().getX());
 	}
 
 	@Test
 	public void test17AdvanceNorth() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "North");
 		sub.action("f");
 		assertEquals(3, sub.getCoordinates().getY());
 	}
 
 	@Test
 	public void test18AdvanceSouth() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "South";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "South");
 		sub.action("f");
 		assertEquals(1, sub.getCoordinates().getY());
 	}
 
 	@Test
 	public void test19AdvanceAfterChangeOfDirection01() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("drf");
         assertEquals(1, sub.getCoordinates().getY());
 	}
 
 	@Test
 	public void test20AdvanceAfterChangeOfDirection02() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "East";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
+		Sub sub = new Sub(new Coordinates(1, 2), "East");
 		sub.action("drrf");
         assertEquals(0, sub.getCoordinates().getX());
 	}
 
 	@Test
 	public void test21ReleaseCapsuleAtInappropriateDepth01() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
-		assertThrowsLike(() -> sub.action("ddm"), DepthDangerousForCapsule.ERROR_MESSAGE);
+		assertThrowsLike(() -> new Sub(new Coordinates(1, 2), "North").action("ddm"), DepthDangerousForCapsule.ERROR_MESSAGE);
 	}
 
 	@Test
 	public void test22ReleaseCapsuleAtInappropriateDepth02() {
-		Coordinates coordenadasIniciales = new Coordinates(1, 2);
-		String direccionInicial = "North";
-		Sub sub = new Sub(coordenadasIniciales, direccionInicial);
-		assertThrowsLike(() -> sub.action("dddm"), DepthDangerousForCapsule.ERROR_MESSAGE);
+		assertThrowsLike(() -> new Sub(new Coordinates(1, 2), "North").action("dddm"), DepthDangerousForCapsule.ERROR_MESSAGE);
 	}
 
-
 	private void assertThrowsLike(Executable executable, String message) {
-
 		assertEquals(message,
 				assertThrows(Exception.class , executable ).getMessage());
 
