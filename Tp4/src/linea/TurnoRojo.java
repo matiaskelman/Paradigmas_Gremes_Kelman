b560b53b4d564173e6986850705b1afb0d1830b6
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:36:06 -0300

    Update TurnoRojo.java

--- Tp4/src/linea/TurnoRojo.java
+++ Tp4/src/linea/TurnoRojo.java
@@ -4,13 +4,9 @@ public class TurnoRojo extends Turno{
 
     public static final String AZUL_NO_PUEDE_PONER_FICHAS_DOS_VECES_SEGUIDAS = "Azul no puede poner fichas dos veces seguidas";
 
-    public char juegaRojo(){
-        return 'R';
-    }
+    public char juegaRojo(){return 'R';}
     
-    public char juegaAzul(){
-        throw new RuntimeException(AZUL_NO_PUEDE_PONER_FICHAS_DOS_VECES_SEGUIDAS);
-    }
+    public char juegaAzul(){throw new RuntimeException(AZUL_NO_PUEDE_PONER_FICHAS_DOS_VECES_SEGUIDAS);}
 
     public char conseguirFicha(){return 'R';}
     
