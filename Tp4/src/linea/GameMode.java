9ea2cc1dd277e5c82e51a3593e4377c0d66e850e
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:30:07 -0300

    Update GameMode.java

--- Tp4/src/linea/GameMode.java
+++ Tp4/src/linea/GameMode.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public abstract class GameMode {
 
+    public static final String NO_EXISTE_ESTE_MODO_DE_JUEGO = "No existe este modo de juego";
     private static List<GameMode> posiblesModoDeJuego = Arrays.asList(new GameMode_A(),new GameMode_B(), new GameMode_C());
 
     public char key;
@@ -15,6 +16,8 @@ public abstract class GameMode {
     public static GameMode modoFor(char modo) {
         List<GameMode> modoGottenFromFilter =
                 posiblesModoDeJuego.stream().filter(c -> modo == c.key).collect(Collectors.toList());
+
+        if(modoGottenFromFilter.isEmpty()){throw new RuntimeException(NO_EXISTE_ESTE_MODO_DE_JUEGO);}
 
         return modoGottenFromFilter.get(0);
     }
