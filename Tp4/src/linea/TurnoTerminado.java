fc0a9857f5791d3e1bcac789b885269c98eac993
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:37:16 -0300

    Update TurnoTerminado.java

--- Tp4/src/linea/TurnoTerminado.java
+++ Tp4/src/linea/TurnoTerminado.java
@@ -2,18 +2,15 @@ package linea;
 
 public class TurnoTerminado extends Turno{
 
+    public static final String JUEGO_YA_HA_TERMINADO = "El juego ya ha terminado";
 
-    public char juegaRojo(){throw new RuntimeException("El juego ya ha terminado");}
-    public char juegaAzul(){throw new RuntimeException("El juego ya ha terminado");}
-
-
-     public char conseguirFicha(){return 'N';}
-     public String getTurno() {
-         return "Juego terminado";
-     }
-    @Override
-    public boolean gano(Linea juego) {
-        return false;
+    public char juegaRojo(){throw new RuntimeException(JUEGO_YA_HA_TERMINADO);}
+    
+    public char juegaAzul(){throw new RuntimeException(JUEGO_YA_HA_TERMINADO);}
+   
+    public char conseguirFicha(){return 'N';}
+    
+    public String getTurno(String ganador) {
+        return ganador;
     }
-
 }
