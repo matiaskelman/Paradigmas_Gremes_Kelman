4cd9735d47a5d1b5dc58f410921edaa351069d5f
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:30:48 -0300

    Update GameMode_A.java

--- Tp4/src/linea/GameMode_A.java
+++ Tp4/src/linea/GameMode_A.java
@@ -1,12 +1,5 @@
 package linea;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
 public class GameMode_A extends GameMode {
 
     {this.key = 'A';}
@@ -15,6 +8,3 @@ public class GameMode_A extends GameMode {
     public boolean isJuegoGanado(Linea juego) {
         return juego.victoriaHorizontal() || juego.victoriaVertical();}
     }
-
-
-
