b3a5a053c8bf0b64b879a10df34d606ad0bae912
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:31:46 -0300

    Update GameMode_B.java

--- Tp4/src/linea/GameMode_B.java
+++ Tp4/src/linea/GameMode_B.java
@@ -1,10 +1,9 @@
 package linea;
 
-public class GameMode_B extends GameMode{
+public class GameMode_C extends GameMode {
 
-    {this.key = 'B';}
+    {this.key = 'C';}
 
     @Override
-    public boolean isJuegoGanado(Linea juego) {return juego.victoriaDiagonalCuadrante1y3() || juego.victoriaDiagonalCuadrante2y4();}
-
-    }
+    public boolean isJuegoGanado(Linea juego) {return juego.victoriaVertical() || juego.victoriaHorizontal() || juego.victoriaDiagonalCuadrante1y3() || juego.victoriaDiagonalCuadrante2y4();}
+}
