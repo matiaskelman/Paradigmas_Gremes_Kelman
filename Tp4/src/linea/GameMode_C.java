fba3513b3871fd94f705137ecccea890d892d038
IgnacioGremes <126919138+IgnacioGremes@users.noreply.github.com>
2023-11-12 16:32:49 -0300

    Update GameMode_C.java

--- Tp4/src/linea/GameMode_C.java
+++ Tp4/src/linea/GameMode_C.java
@@ -1,12 +1,8 @@
 package linea;
 
-import java.util.ArrayList;
-
 public class GameMode_C extends GameMode {
 
-    {
-        this.key = 'C';
-    }
+    {this.key = 'C';}
 
     @Override
     public boolean isJuegoGanado(Linea juego) {return juego.victoriaVertical() || juego.victoriaHorizontal() || juego.victoriaDiagonalCuadrante1y3() || juego.victoriaDiagonalCuadrante2y4();}
